22beb9585d2482c76ee8d030beae981acf9faaaf
Katie <105233452+stylist14@users.noreply.github.com>
2022-07-20 03:05:48 +0000

    Add answer alerts, Update CSS Styling

---
+++ script.js
@@ -0,0 +1,61 @@
+<script>
+    document.getElementById("q1-a1").addEventListener("click", function(){alert("🎉 Correct!")});
+    document.getElementById("q1-a2").addEventListener("click", function(){ alert("Try Again. Remember 'sans serif' means 'without' decorative lines at the end of the stroke.")});
+    document.getElementById("q1-a3").addEventListener("click", function(){ alert("Try Again. Pay attention to the lines in the ends of the strokes.")});
+    document.getElementById("q1-a4").addEventListener("click", function(){ alert("Try Again. Are there decorative lines at the end of the strokes?")});
+   
+    document.getElementById("q2-a1").addEventListener("click", function(){alert("🎉 Correct!")});
+    document.getElementById("q2-a2").addEventListener("click", function(){ alert("Not this one. The letter sides are straight up and don't need special attention.")});
+    document.getElementById("q2-a3").addEventListener("click", function(){ alert("🎉 Correct!")});
+    document.getElementById("q2-a4").addEventListener("click", function(){ alert("Not this one. Try again.")});
+
+    document.getElementById("q3-a1").addEventListener("click", function(){alert("Try again. To achieve contrast, the styles should be VERY different.")});
+    document.getElementById("q3-a2").addEventListener("click", function(){ alert("🎉Right! These fonts are similar but slightly different, they conflict")});
+
+    document.getElementById("q4-a1").addEventListener("click", function(){alert("🎉Correct!")});
+    document.getElementById("q4-a2").addEventListener("click", function(){ alert("Try again")});
+
+    document.getElementById("q5-a1").addEventListener("click", function(){alert("🎉Correct!")});
+    document.getElementById("q5-a2").addEventListener("click", function(){ alert("Try again")});
+
+
+    document.getElementById("q6-a1").addEventListener("click", function(){alert("Try again")});
+    document.getElementById("q6-a2").addEventListener("click", function(){ alert("🎉Correct!")});
+    
+    document.getElementById("q7-a1").addEventListener("click", function(){alert("Try again")});
+    document.getElementById("q7-a2").addEventListener("click", function(){ alert("🎉Correct!")});
+
+    document.getElementById("q8-a1").addEventListener("click", function(){alert("🎉Correct!")});
+    document.getElementById("q8-a2").addEventListener("click", function(){ alert("Try again")});
+
+    document.getElementById("q9-a1").addEventListener("click", function(){alert("🎉Correct!")});
+    document.getElementById("q9-a2").addEventListener("click", function(){ alert("Try again")});
+
+    document.getElementById("q10-a1").addEventListener("click", function(){alert("🎉Correct!")});
+    document.getElementById("q10-a2").addEventListener("click", function(){ alert("Try again")});
+
+    document.getElementById("q11-a1").addEventListener("click", function(){alert("Try again")});
+    document.getElementById("q11-a2").addEventListener("click", function(){ alert("🎉Correct!")});
+
+    document.getElementById("q13-a1").addEventListener("click", function(){alert("🎉Correct!")});
+    document.getElementById("q13-a2").addEventListener("click", function(){ alert("Try again")});
+
+    document.getElementById("q14-a1").addEventListener("click", function(){alert("🎉Correct!")});
+    document.getElementById("q14-a2").addEventListener("click", function(){ alert("Try again")});
+
+    document.getElementById("q15-a1").addEventListener("click", function(){alert("Try again")});
+    document.getElementById("q15-a2").addEventListener("click", function(){ alert("🎉Correct!")});
+
+    document.getElementById("q16-a1").addEventListener("click", function(){alert("Try again")});
+    document.getElementById("q16-a2").addEventListener("click", function(){ alert("🎉Correct!")});
+
+    document.getElementById("q17-a1").addEventListener("click", function(){alert("🎉Correct!")});
+    document.getElementById("q17-a2").addEventListener("click", function(){ alert("Try again")});
+
+    document.getElementById("q18-a1").addEventListener("click", function(){alert("🎉Correct!")});
+    document.getElementById("q18-a2").addEventListener("click", function(){ alert("Try again")});
+
+    document.getElementById("q19-a1").addEventListener("click", function(){alert("🎉Correct!")});
+    document.getElementById("q19-a2").addEventListener("click", function(){ alert("Try again")});
+    
+   </script>
